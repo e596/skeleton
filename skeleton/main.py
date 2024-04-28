cc4f22b581bc264d215d214bcaa47a307d0c0bef
e596 <66113654+e596@users.noreply.github.com>
2024-04-28 17:52:19 +0500

    Update main.py

--- skeleton/main.py
+++ skeleton/main.py
@@ -12,14 +12,18 @@ player_left = pygame.transform.flip(player, True, False)
 background = pygame.image.load('images/248880a15879483281825de6f0b4c0f4HEqGnLzIi2JXz9nc-0.png')
 health = pygame.image.load('images/360_F_487872099_QimOSUlugZRHlny2jgFHaCy5R7m0UYmT.png')
 
-player_speed = 2
+player_speed = 5
 player_x = 0
+player_y = 0
+
+is_jump = False
+jump_count = 5
 
 running = True
 while running:
 
     screen.blit(background, (0, 0))
-    screen.blit(player, (player_x, 0))
+    screen.blit(player, (player_x, player_y))
     screen.blit(health, (-270, 300))
 
     keys = pygame.key.get_pressed()
@@ -29,6 +33,20 @@ while running:
     elif keys[pygame.K_RIGHT]:
         player_x += player_speed
         player = player_right
+
+    if not is_jump:
+     if keys[pygame.K_UP]:
+        is_jump = True
+    else:
+        if jump_count >= -5:
+            if jump_count > 0:
+                player_y -= (jump_count ** 2) / 2
+            else:
+                player_y += (jump_count ** 2) / 2
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 5
 
     pygame.display.update()
 
